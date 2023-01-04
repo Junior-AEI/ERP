b9266d4b3c86b0beee75eed73af72d1f1863b655
maelpaul <maelpaul@orange.fr>
2023-01-04 10:08:24 +0100

    modif suppr.sh

--- suppr.sh
+++ suppr.sh
@@ -1,3 +1,4 @@
+git restore doc/
 rm -f doc/workspace-papyrus/.metadata/.plugins/org.eclipse.core.resources/.root/$1.tree 
 rm -f doc/workspace-papyrus/.metadata/.plugins/org.eclipse.core.resources/.safetable/org.eclipse.papyrus.infra.emf.$1
 rm -f doc/workspace-papyrus/.metadata/.plugins/org.eclipse.papyrus.infra.emf/index/PapyrusWorkspaceProfiles.$1

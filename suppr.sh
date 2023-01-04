7096236e9d4fc431e7526f4c7af6afb65e18952f
maelpaul <maelpaul@orange.fr>
2023-01-04 10:03:28 +0100

    suppr.sh

---
+++ suppr.sh
@@ -0,0 +1,5 @@
+rm -f doc/workspace-papyrus/.metadata/.plugins/org.eclipse.core.resources/.root/$1.tree 
+rm -f doc/workspace-papyrus/.metadata/.plugins/org.eclipse.core.resources/.safetable/org.eclipse.papyrus.infra.emf.$1
+rm -f doc/workspace-papyrus/.metadata/.plugins/org.eclipse.papyrus.infra.emf/index/PapyrusWorkspaceProfiles.$1
+rm -f doc/workspace-papyrus/.metadata/.plugins/org.eclipse.papyrus.infra.emf/index/papyrusCrossRefs.$1
+rm -f doc/workspace-papyrus/.metadata/.plugins/org.eclipse.papyrus.infra.emf/index/papyrusUMLProfiles.$1

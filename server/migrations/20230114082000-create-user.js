c17bf08ccb88c89ca486fb40fe55b5e8c2b8b947
VOVARD Marine <Marine.Vovard@bordeaux-inp.fr>
2023-01-14 09:37:25 +0100

    :construction: Started migration system

---
+++ server/migrations/20230114082000-create-user.js
@@ -0,0 +1,34 @@
+'use strict';
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('users', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      username: {
+        type: Sequelize.STRING
+      },
+      lastConnection: {
+        type: Sequelize.DATE
+      },
+      password: {
+        type: Sequelize.STRING
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('users');
+  }
+};
--- server/src/config/database.config.ts
+++ server/src/config/database.config.ts
@@ -1,7 +1,5 @@
 import sqlite3 from "sqlite3";
 const { Sequelize } = require('sequelize');
-// import Sequelize from "sequelize";
-
 
 
 const db = new sqlite3.Database(":memory:");

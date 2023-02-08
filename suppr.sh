cfc0b18bab22e56e4d255dcf546434abda7dba0a
Antoine Raoult <55808785+AntoineRaoult@users.noreply.github.com>
2023-02-08 18:09:14 +0100

    :fire: delete suppr.sh

--- suppr.sh
+++
@@ -1,6 +0,0 @@
-git restore doc/
-rm -f doc/workspace-papyrus/.metadata/.plugins/org.eclipse.core.resources/.root/$1.tree 
-rm -f doc/workspace-papyrus/.metadata/.plugins/org.eclipse.core.resources/.safetable/org.eclipse.papyrus.infra.emf.$1
-rm -f doc/workspace-papyrus/.metadata/.plugins/org.eclipse.papyrus.infra.emf/index/PapyrusWorkspaceProfiles.$1
-rm -f doc/workspace-papyrus/.metadata/.plugins/org.eclipse.papyrus.infra.emf/index/papyrusCrossRefs.$1
-rm -f doc/workspace-papyrus/.metadata/.plugins/org.eclipse.papyrus.infra.emf/index/papyrusUMLProfiles.$1

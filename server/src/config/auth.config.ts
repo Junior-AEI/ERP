f09973f1189cd904d50593252169e147d4753f80
AntoineRaoult <antoine-raoult@orange.fr>
2023-02-09 14:45:10 +0100

    merge main

--- server/src/config/auth.config.ts
+++ server/src/config/auth.config.ts
@@ -1,9 +1,11 @@
 import { createSecretKey } from "crypto";
+import dotenv from "dotenv";
 
-const JWT_ISSUER = "AEI";
+dotenv.config();
+
+const JWT_ISSUER = process.env.JWT_ISSUER as string;
 const JWT_AUDIENCE = "*";
-const JWT_SECRET =
-  "6wEpZ$4qx*E8eK8@vsm2Qt6k93nYMNTi*WU#c*&#B4a2QvxB*L8LuNvVF@rzR%@4J#E2Tv#2cYxmd$6&ExDPX2jRR*5P^4vb*p4ysi&wqpqCpGn2K*9mdCDnA6AH7bt5";
+const JWT_SECRET = process.env.JWT_SECRET as string;
 const JWT_EXPIRATION = "1 week";
 const JWT_SECRET_KEY = createSecretKey(JWT_SECRET as string, "utf-8");
 

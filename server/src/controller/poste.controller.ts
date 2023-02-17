315f6394b94a37350472ccf25f09315137b6e257
DUPOUX Mathieu <Mathieu.Dupoux@bordeaux-inp.fr>
2023-02-17 17:27:50 +0100

    🚧 ♻️ Refactor check functions for "poste" routes

--- server/src/controller/poste.controller.ts
+++ server/src/controller/poste.controller.ts
@@ -1,12 +1,40 @@
 import { Request, Response } from "express";
 import { Poste } from "../models/poste.model";
 import { Op } from "sequelize";
+import { truncate } from "lodash";
 
 const posteController = {
   getAllPostes,
   getPosteById,
   createPoste,
+  // updatePoste,
 };
+
+// TODO: Setup validator ("express-validator" package?) to verify whole body
+/**
+ * Avoid empty role name
+ */
+async function checkEmptyName(req: Request) {
+  if (
+    req.body.nom === "" ||
+    req.body.nom === undefined ||
+    req.body.nom === null
+  ) {
+    throw new Error("Empty name provided");
+  }
+}
+
+/**
+ * Avoid creating existing role
+ */
+async function checkExistingPoste(req: Request): Promise<void> {
+  const existingPoste = await Poste.findOne({
+    where: {
+      [Op.or]: [{ nom: req.body.nom }, { description: req.body.description }],
+    },
+  });
+  if (existingPoste !== null) throw new Error("Role already exist");
+}
 
 async function getAllPostes(req: Request, res: Response) {
   await Poste.findAll().then((poste) => res.json(poste));
@@ -18,32 +46,29 @@ async function getPosteById(req: Request, res: Response) {
     .catch((err) => res.status(500).json({ error: err.message }));
 }
 
+/**
+ * Role creation for POST route
+ * @param req Request
+ * @param res Reply
+ */
 async function createPoste(req: Request, res: Response) {
-  // Avoid void role name
-  if (req.body.nom === "" || undefined || null) {
-    return res.status(400).json({ error: "Empty role name provided" });
+  try {
+    await checkEmptyName(req);
+    await checkExistingPoste(req);
+
+    // Clean useless creation and update dates if given (setup while creating role)
+    req.body.createdAt = null;
+    req.body.updatedAt = null;
+
+    await Poste.create(req.body)
+      .then((poste) => res.status(200).json(poste))
+      .catch((err) => res.status(500).json(err));
+  } catch (err: any) {
+    res.status(401).json({
+      status: "error",
+      message: err.message,
+    });
   }
-
-  // Avoid creating existing role
-  const existingPoste = await Poste.findOne({
-    where: {
-      [Op.or]: [{ nom: req.body.nom }, { description: req.body.description }],
-    },
-  });
-  if (existingPoste !== null) {
-    return res.status(400).json({ error: "This role seems to already exist" });
-  }
-
-  // Reset dates if specified
-  req.body.createdAt = null;
-  req.body.updatedAt = null;
-
-  // TODO: Setup validator ("express-validator" package?) to verify whole body
-
-  // Create role
-  await Poste.create(req.body)
-    .then((poste) => res.json(poste))
-    .catch((err) => res.status(500).json({ error: err.message }));
 }
 
 export default posteController;

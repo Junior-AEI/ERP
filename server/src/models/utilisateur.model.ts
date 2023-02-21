1568c5410825288c07c528ae2870a5a499904850
AntoineRaoult <antoine-raoult@orange.fr>
2023-02-21 11:46:08 +0100

    :sparkles: register/login

---
+++ server/src/models/utilisateur.model.ts
@@ -0,0 +1,23 @@
+import { DataTypes, Model } from "sequelize";
+import { sequelize } from "../config/database.config";
+
+class Utilisateur extends Model {
+  declare nomUtilisateur: string;
+  declare estActive: boolean;
+  declare derniereConnexion: Date;
+  declare motDePasse: string;
+}
+
+Utilisateur.init(
+  {
+    nomUtilisateur: DataTypes.STRING,
+    estActive: DataTypes.BOOLEAN,
+    derniereConnexion: DataTypes.DATE,
+    motDePasse: DataTypes.STRING,
+  },
+  { sequelize }
+);
+
+sequelize.sync();
+
+export { Utilisateur };

b6b747d582657b6729a0e5b8fd62ca571469224c
Mathias ROSA <mathias.rosa@enseirb-matmeca.fr>
2024-02-26 01:43:19 +0100

    💄 changement du gap dans la sidebar

--- app/src/components/sidebar/ExpandedSidebar.vue
+++ app/src/components/sidebar/ExpandedSidebar.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="flex w-full flex-col gap-5 border-r bg-primary-foreground px-6 py-5 sm:w-80">
+  <div class="flex w-full flex-col gap-3 border-r bg-primary-foreground px-6 py-5 sm:w-80">
     <div class="flex w-full justify-between">
       <h3>Hugo Bastien</h3>
       <Icon name="chevron_left" variant="clickable" @click="$emit('reduce')" />

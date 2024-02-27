71a489c4eb694fe99f007c52acd24709c6ff7542
Mathias ROSA <mathias.rosa@enseirb-matmeca.fr>
2024-02-27 22:40:05 +0100

    💄 Fix du header sur chromium

--- app/src/components/Titlebar.vue
+++ app/src/components/Titlebar.vue
@@ -1,12 +1,12 @@
 <template>
-  <div class="flex h-10 w-full justify-between border-b bg-primary-foreground px-4">
+  <div class="flex h-12 w-full items-center justify-between border-b bg-primary-foreground px-4">
     <div class="flex items-center gap-2 text-sm">
       <Icon :name="icon" />
       <span class="text-primary">
         {{ routeName }}
       </span>
     </div>
-    <img src="/logo.svg" alt="logo" />
+    <img src="/logo.svg" alt="logo" class="h-full" />
   </div>
 </template>
 

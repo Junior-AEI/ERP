3a5f2659cbf716c562b43b86bd10bb47806f4373
Paul BARBARIN <contact@paulbarbarin.com>
2024-04-08 12:30:14 +0200

    Review auth

--- server/src/middlewares/auth.middleware.ts
+++ server/src/middlewares/auth.middleware.ts
@@ -1,19 +1,7 @@
-// Copyright (C) 2023 Nesrine ABID, Nadjime BARTEAU, Mathieu DUPOUX, Léo-Paul MAZIÈRE, Maël PAUL, Antoine RAOULT, Lisa VEILLAT, Marine VOVARD
-
-// Authors: Nesrine ABID, Nadjime BARTEAU, Mathieu DUPOUX, Léo-Paul MAZIÈRE, Maël PAUL, Antoine RAOULT, Lisa VEILLAT, Marine VOVARD
-// Maintainer: contact@junior-aei.com
-
-// This file is part of LATIME.
-
-// LATIME is free software: you can redistribute it and/or modify it under the terms of the GNU Affero General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
-
-// LATIME is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for more details.
-
-// You should have received a copy of the GNU Affero General Public License along with LATIME. If not, see <https://www.gnu.org/licenses/>.
 import { Request, Response } from 'express'
 import { jwtVerify } from 'jose'
 import { JWT_SECRET_KEY } from '../config/auth.config'
-import Utilisateur from '../models/utilisateur.model'
+import Users from '../models/user.model'
 
 /**
  * TODO : Comment
@@ -46,9 +34,9 @@ const getUsername = async (req: Request, res: Response, next: () => void) => {
             const username = payload.username
 
             // Fetch user
-            const user = await Utilisateur.findOne({
+            const user = await Users.findOne({
                 where: {
-                    nomUtilisateur: username
+                    username: username
                 }
             })
 
@@ -56,7 +44,7 @@ const getUsername = async (req: Request, res: Response, next: () => void) => {
             if (user) {
                 res.locals.user = {
                     id: user.id,
-                    nomUtilisateur: user.nomUtilisateur
+                    username: user.username
                 }
 
                 next()

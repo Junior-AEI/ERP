a0f8b124659ba1a04dd929b7dc21f7a1c8361674
Mathieu MOREL <Mathieu.Morel@bordeaux-inp.fr>
2024-04-08 20:17:28 +0200

    Beginning test /api/new-password

--- server/tests/auth.test.ts
+++ server/tests/auth.test.ts
@@ -1,12 +1,12 @@
 const request = require('supertest')
-
 import { sequelizeInit, sequelizeClose } from '../src/config/database.config'
-
+import bcrypt from 'bcrypt'
 import app from "../src/app";
 import { createUser } from './seeders/user.seeders';
 import { clearDatabase } from './utils';
 import Tokens from '../src/models/token.model';
 import Users from '../src/models/user.model';
+import { promisify } from 'util';
 
 beforeAll(async () => {
     await sequelizeInit()
@@ -118,22 +118,111 @@ describe('ROUTE: /api/forget', () => {
         expect(res.status).toEqual(200);
         expect(res.body.status).toEqual("success");
         expect(res.body.data.token).toBeDefined();
-        
+
         const user = await Users.findOne({
             where: {
                 username: "john.doe"
             }
         })
-        
+
         const tokenDb = await Tokens.findOne({
             where: {
                 userId: user?.userId
             }
         })
-        
+
         // Token has been recorded in the db
         expect(res.body.data.token).toEqual(tokenDb?.token);
 
+    })
+
+})
+
+describe('ROUTE: /api/new-password', () => {
+
+    afterEach(clearDatabase);
+
+    // ! 
+    // it('Wrong informations', async () => {
+
+    //     await createUser();
+
+    //     const wrongInformationsList = [
+    //         // Void
+    //         {
+    //             token: null,
+    //             newPassword: null,
+    //         },
+    //         // Empty
+    //         {
+    //             token: "",
+    //             newPassword: "",
+    //         },
+    //         // Wrong token
+    //         {
+    //             token: "wrongToken",
+    //             newPassword: "randomPassword",
+    //         },
+    //         // ...
+    //         // TODO : Bad new passwords 
+    //     ]
+
+    //     wrongInformationsList.map(async (wrongInformations) => {
+    //         const res = await request(app)
+    //             .post('/api/new-password')
+    //             .send(wrongInformations)
+    //         expect(res.body.status).toEqual("error");
+    //     })
+
+    // })
+    // ! 
+
+    it('Good usage', async () => {
+
+        await createUser();
+
+        let user = await Users.findOne({
+            where: {
+                username: "john.doe"
+            }
+        })
+
+        const oldPasswordHash = user?.password ? user.password : ""
+
+        expect(await promisify(bcrypt.compare)('mdp', oldPasswordHash)).toBeTruthy();
+        
+        const token = "123456789randomTokenAZERTY"
+        const currentDate = new Date()
+        const tenMinutesLater = new Date(currentDate.getTime() + 10 * 60000)
+        
+        await Tokens.create({
+            token: token,
+            validity: tenMinutesLater,
+            userId: user?.userId
+        })
+        
+        const res = await request(app)
+        .post('/api/new-password')
+        .send({
+            token: token,
+            password: "validPassword",
+        })
+        
+        expect(res.status).toEqual(200);
+        expect(res.body.status).toEqual("success");
+        
+        user = await Users.findOne({
+            where: {
+                username: "john.doe"
+            }
+        })
+        
+        const newPasswordHash = user?.password ? user.password : ""
+        
+        expect(newPasswordHash).not.toEqual(oldPasswordHash)
+        expect(await promisify(bcrypt.compare)('mdp', newPasswordHash)).toBeFalsy();
+        expect(await promisify(bcrypt.compare)('validPassword', newPasswordHash)).toBeTruthy();
+        
     })
 
 })

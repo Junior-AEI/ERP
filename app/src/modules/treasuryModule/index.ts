e8d0cf6186dfa0788ea34d327ebc1088c15c4743
Bastien Hugo <hbastien@enseirb-matmeca.fr>
2024-04-10 17:08:21 +0200

    Début de la vue Trésorerie et création des components project et tasks

--- app/src/modules/treasuryModule/index.ts
+++ app/src/modules/treasuryModule/index.ts
@@ -6,7 +6,16 @@ export const treasuryModule: Module = {
     {
       path: '/treasury',
       name: 'Trésorerie',
-      component: () => import('@/views/Construction.vue'),
+      component: () => import('./views/DashBoardTreasuryView.vue'),
+      children: [
+        {
+          path: 'carte-d-aei',
+          name: "Codes de carte D'AEI",
+          component: () => import('@/views/Construction.vue'),
+          meta: {
+            icon: 'credit_card'
+          }
+        }],
       meta: {
         icon: 'savings'
       }

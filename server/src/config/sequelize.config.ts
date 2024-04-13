70907967f4c2f4de76e0758a47a4dd75d0969a5d
Mathias ROSA <mat.ros@outlook.fr>
2024-04-13 11:47:35 +0200

    💚 Prod Database now create tables but are empty

--- server/src/config/sequelize.config.ts
+++ server/src/config/sequelize.config.ts
@@ -24,6 +24,7 @@ const configuration: { [key: string]: SequelizeOptions } = {
 
     // Prod database server configuration (from .env file)
     production: {
+        models: [__dirname + '/../models'],
         username: process.env.DB_USER,
         password: process.env.DB_PASSWORD,
         database: process.env.DB_NAME,

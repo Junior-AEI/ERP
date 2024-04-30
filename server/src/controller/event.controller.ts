34f9f6372be5a856a9e8079111cf3b411d346a52
Paul BARBARIN <contact@paulbarbarin.com>
2024-04-30 18:38:52 +0200

    tests of events

--- server/src/controller/event.controller.ts
+++ server/src/controller/event.controller.ts
@@ -33,11 +33,10 @@ const getAll = async (req: Request, res: Response) => {
  */
 const getByPk = async (req: Request, res: Response) => {
     try {
-        if (req.params.eventId && !isNumber(req.params.eventId)) throw createHttpError(400, 'Please provide a valid identifier')
-
         const identifier = parseInt(req.params.eventId)
+        if (isNaN(identifier)) throw createHttpError(400, 'Please provide a valid identifier')
 
-        const event = await Events.findByPk(identifier, {})
+        const event = await Events.findByPk(identifier)
 
         if (!event) throw createHttpError(404, 'event not found')
 
@@ -60,23 +59,21 @@ const getByPk = async (req: Request, res: Response) => {
  */
 async function create(req: Request, res: Response) {
     try {
-        // Parse identifier for member heredity
-        if (req.body.user.eventId && !isNumber(req.body.user.eventId)) throw createHttpError(400, 'Please provide a valid identifier for the linked member.')
-        const identifier = parseInt(req.body.user.eventId)
 
         // Test params
-        const validator = isValidEvent(req.body.event.name, req.body.event.startDate, req.body.event.endDate, req.body.event.location, req.body.event.description)
-        if (!validator.valid) return createHttpError(400, validator.message as string)
+        const validator = isValidEvent(req.body.event.name, req.body.event.startDate, req.body.event.endDate, req.body.event.location, req.body.event.description, req.body.event.eventTypeName)
+
+        if (!validator.valid) throw createHttpError(400, validator.message as string)
 
         // Insert data
         const event = await Events.create({
-            eventId: identifier,
             name: req.body.event.name,
             startDate: req.body.event.startDate,
             endDate: req.body.event.endDate,
             location: req.body.event.location,
-            description: req.body.event.description
-        })
+            description: req.body.event.description,
+            eventTypeName: req.body.event.eventTypeName
+        });
 
         // Return success
         return res.status(200).json({
@@ -85,6 +82,7 @@ async function create(req: Request, res: Response) {
                 eventId: event.eventId
             }
         })
+
     } catch (err) {
         if (err instanceof HttpError) controllerErrorHandler(err, res)
         else throw err
@@ -98,17 +96,17 @@ async function create(req: Request, res: Response) {
  */
 const update = async (req: Request, res: Response) => {
     try {
-        if (req.params.eventId && !isNumber(req.params.eventId)) throw createHttpError(400, 'Please provide a valid identifier')
         const identifier = parseInt(req.params.eventId)
+        if (isNaN(identifier)) throw createHttpError(400, 'Please provide a valid identifier')
 
-        const event = await Events.findByPk(identifier, {})
-        if (!event) throw createHttpError(404, 'event not found')
+        const event = await Events.findByPk(identifier)
+        if (!event) throw createHttpError(404, 'Event not found')
 
         // Test params
-        const validator = isValidEvent(req.body.event.name, req.body.event.startDate, req.body.event.endDate, req.body.event.location, req.body.event.description)
-        if (!validator.valid) return createHttpError(400, validator.message as string)
+        const validator = isValidEvent(req.body.event.name, req.body.event.startDate, req.body.event.endDate, req.body.event.location, req.body.event.description, req.body.event.eventTypeName)
+        if (!validator.valid) throw createHttpError(400, validator.message as string)
 
-        await Events.update(req.body, {
+        await Events.update(req.body.event, {
             where: { eventId: identifier }
         })
 
@@ -128,17 +126,22 @@ const update = async (req: Request, res: Response) => {
  */
 const del = async (req: Request, res: Response) => {
     try {
-        if (req.params.eventId && !isNumber(req.params.eventId)) throw createHttpError(400, 'Please provide a valid identifier')
         const identifier = parseInt(req.params.eventId)
-
-        const event = await Events.findByPk(identifier, {})
-        if (!event) throw createHttpError(404, 'event not found')
-
+        if (isNaN(identifier)) throw createHttpError(400, 'Please provide a valid identifier')
+        
+        const event = await Events.findByPk(identifier)
+        if (!event) throw createHttpError(404, 'Event not found')
+        
         await Events.destroy({
             where: {
                 eventId: identifier
             }
         })
+
+        return res.status(200).json({
+            status: 'success'
+        });
+
     } catch (err) {
         if (err instanceof HttpError) controllerErrorHandler(err, res)
         else throw err

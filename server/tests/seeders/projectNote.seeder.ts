d42ad459a1978afc38d1cf1c23e620c40d9de0fe
fushikawaa <jules.braun38@gmail.com>
2024-05-02 11:26:54 +0200

    Project note test done - Jules BRAUN

---
+++ server/tests/seeders/projectNote.seeder.ts
@@ -0,0 +1,20 @@
+import ProjectNotes from "../../src/models/projectNote.model";
+import { projectNotes } from "./data/projectNotes.data";
+import { createProject } from "./project.seeder";
+import { createUser } from "./user.seeders";
+
+export const createProjectNote = async (n: number) => {
+    const note = "note" + n;
+    const projectId = await createProject(projectNotes[note].project.acronym, projectNotes[note].project.username)
+
+    const userId = await createUser('john.doe')
+    
+    const projectNote = await ProjectNotes.create({
+        projectId: projectId,
+        writerId: userId,
+        comment: projectNotes[note].comment,
+        advancement: projectNotes[note].advancement,
+    })
+
+    return projectNote.noteId
+}

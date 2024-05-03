2c38a4f2d245bcbd1db8fb08d052cfd13e0a7a9c
Paul BARBARIN <contact@paulbarbarin.com>
2024-05-03 16:23:04 +0200

    tests

--- server/src/controller/utils.controller.ts
+++ server/src/controller/utils.controller.ts
@@ -38,6 +38,7 @@ export async function controllerErrorHandler(err: HttpError, res: Response) {
 
 
 export const sendEmail = async (to: string, subject: string, text: string, from : string = 'erp-mail@junior-aei.com') => {
+    if(process.env.NODE_ENV === 'test') return;
     try {
         const mailApiUrl = "127.0.0.1:" + process.env.MAIL_PORT;
         await axios.options(`${mailApiUrl}/mail`);
@@ -58,6 +59,7 @@ export const sendEmail = async (to: string, subject: string, text: string, from 
 
 
 export const sendBotMesssage = async (chatID: number, message: string,) => {
+    if(process.env.NODE_ENV === 'test') return;
     try {
         const botApiUrl = process.env.BOT_API_URL;
         await axios.options(`${botApiUrl}/send`);

de0f4e5f7e1afb50d1a40746f47f19a17af755bf
Corentin Domas <corentin.domas@bordeaux-inp.fr>
2024-05-10 11:53:46 +0200

    Review Dashboard frontend

--- app/src/components/reviews/ReviewsDashboard.vue
+++ app/src/components/reviews/ReviewsDashboard.vue
@@ -1,26 +1,65 @@
 <template>
   <div>
-    <Wrapper class="w-full">
-      <Card>
-        <CardHeader> Relectures à effectuer </CardHeader>
-        <CardContent class="items-center justify-center text-6xl">
-          {{ dataLength }}
-        </CardContent>
-      </Card>
-      <Card>
-        <CardHeader> Suivi d'études </CardHeader>
-        <CardContent>
-          <div class="grid grid-cols-3 gap-2 text-center">
-            <Card class="size-14 rounded-md"> ABC1 </Card>
-            <Card class="size-14 rounded-md"> ABC2 </Card>
-            <Card class="size-14 rounded-md"> DEF </Card>
-          </div>
-        </CardContent>
-      </Card>
-      <Card>
-        <CardHeader> Dernières relectures </CardHeader>
-        <CardContent> </CardContent>
-      </Card>
+    <Wrapper class="h-full w-full">
+      <div class="mt-0">
+        <Card class="flex h-full flex-col justify-between">
+          <CardHeader class="font-semibold"> Relectures à effectuer </CardHeader>
+          <CardContent class="flex items-center justify-center text-9xl">
+            {{ dataLength }}
+          </CardContent>
+        </Card>
+      </div>
+      <div class="mx-4 w-px bg-gray-300"></div>
+      <div>
+        <h2 class="mb-2">Suivi d'études</h2>
+        <div class="grid grid-cols-3 gap-2 text-center">
+          <Card class="size-14 rounded-md"> ABC1 </Card>
+          <Card class="size-14 rounded-md"> ABC2 </Card>
+          <Card class="size-14 rounded-md"> DEF </Card>
+        </div>
+      </div>
+      <div class="mx-4 w-px bg-gray-300"></div>
+      <div class="h-36 w-1/2">
+        <h2 class="mb-2">Dernières relectures</h2>
+        <ScrollArea class="h-36 gap-4 rounded border bg-white">
+          <Card>
+            <div class="m-4 ml-4 mr-4 flex flex-1 flex-row-reverse items-center gap-4">
+              <div
+                class="absolute left-0 top-0 flex h-full w-1/6 items-center justify-center rounded-s-xl bg-black/30"
+              >
+                ABC1
+              </div>
+              <div class="flex w-5/6 flex-col items-start justify-center">
+                <span>Convention d'Étude</span>
+              </div>
+            </div>
+          </Card>
+          <Card>
+            <div class="m-4 ml-4 mr-4 flex flex-1 flex-row-reverse items-center gap-4">
+              <div
+                class="absolute left-0 top-0 flex h-full w-1/6 items-center justify-center rounded-s-xl bg-black/30"
+              >
+                ABC2
+              </div>
+              <div class="flex w-5/6 flex-col items-start justify-center">
+                <span>Avenant à la convention d'Étude</span>
+              </div>
+            </div>
+          </Card>
+          <Card>
+            <div class="m-4 ml-4 mr-4 flex flex-1 flex-row-reverse items-center gap-4">
+              <div
+                class="absolute left-0 top-0 flex h-full w-1/6 items-center justify-center rounded-s-xl bg-black/30"
+              >
+                ABC1
+              </div>
+              <div class="flex w-5/6 flex-col items-start justify-center">
+                <span>Convention d'Étude</span>
+              </div>
+            </div>
+          </Card>
+        </ScrollArea>
+      </div>
     </Wrapper>
   </div>
   <div>
@@ -36,6 +75,7 @@ import type { Document, DocumentType, DocumentFull } from '@/types/api'
 import axios from 'axios'
 import { useAuthStore } from '@/stores/authStore'
 import { Card, CardContent, CardHeader } from '@/components/ui/card'
+import { ScrollArea } from '@/components/ui/scroll-area'
 
 const emit = defineEmits(['reloaded'])
 
